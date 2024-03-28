734c73d5bb2e6cbc83a3449b24bb3676ea440fa5
Hagar Telles <hagar.telles@gmail.com>
2024-03-28 00:39:29 -0300

    fix/ fix deploy bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { HeroSection} from "@/components/sections/header/HeroSection"
 import { ActionArea } from "@/components/sections/actionArea/ActionArea"
 import { About } from "@/components/sections/about/About"
-import { Treatments } from "@/components/Treatments"
 import  FormComponent  from "@/components/sections/form/FormComponent"
 import  Faqs  from "@/components/sections/faq/Faq"
 
