68ff2f8f4702aaad00910f4168323b06fbe488a0
Hagar Telles <hagar.telles@gmail.com>
2024-05-08 18:15:29 -0300

    deploy/ adjust on form

--- src/components/sections/form/FormComponent.tsx
+++ src/components/sections/form/FormComponent.tsx
@@ -9,7 +9,7 @@ const FormComponent: React.FC = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    telefone:'',
+    telefone: '',
     message: ''
   });
 
@@ -23,76 +23,89 @@ const FormComponent: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(formData); // Aqui você pode fazer o que quiser com os dados do formulário
+    fetch("https://formsubmit.co/ajax/deborahmuller_@hotmail.com", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => response.json())
+      .then(data => alert('Eu recebi a sua mensagem <3'))
+
+      .catch(error => {
+        alert(`Ops, ocorreu um erro no envio: ${error}`);
+      });
   };
 
   return (
     <DivBackground name="Contato">
       <div className="pt-24">
-      <h2 className="text-center text-xl font-bold m-2">Contato</h2>
-      <form className="w-3/4 max-w-lg mx-auto shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-burnt_rose/60" onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-sm font-bold mb-2" htmlFor="name">
-            Nome:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
-            id="name"
-            name="name"
-            type="text"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Nome"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block  text-sm font-bold mb-2 " htmlFor="email">
-            E-mail:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
-            id="email"
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="E-mail"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block  text-sm font-bold mb-2 " htmlFor="email">
-          Telefone:
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
-            id="telefone"
-            name="telefone"
-            type="telefone"
-            value={formData.telefone}
-            onChange={handleChange}
-            placeholder="telefone"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block text-sm font-bold mb-2 " htmlFor="message">
-            Mensagem:
-          </label>
-          <textarea
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            placeholder="Mensagem"
-            required
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="
+        <h2 className="text-center text-xl font-bold m-2">Contato</h2>
+        <form className="w-3/4 max-w-lg mx-auto shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-burnt_rose/60" action="https://formsubmit.co/hagar.telles@email.com" method="POST" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-sm font-bold mb-2" htmlFor="name">
+              Nome:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
+              id="name"
+              name="name"
+              type="text"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Nome"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block  text-sm font-bold mb-2 " htmlFor="email">
+              E-mail:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
+              id="email"
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="E-mail"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block  text-sm font-bold mb-2 " htmlFor="email">
+              Telefone:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
+              id="telefone"
+              name="telefone"
+              type="telefone"
+              value={formData.telefone}
+              onChange={handleChange}
+              placeholder="telefone"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-sm font-bold mb-2 " htmlFor="message">
+              Mensagem:
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              placeholder="Mensagem"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="
             w-full 
             bg-misty_rose
              hover:bg-beige 
@@ -104,12 +117,12 @@ const FormComponent: React.FC = () => {
 
              shadow-lg shadow-black/30
              "
-            type="submit"
-          >
-            Enviar
-          </button>
-        </div>
-      </form>
+              type="submit"
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
       </div>
     </DivBackground>
   );
